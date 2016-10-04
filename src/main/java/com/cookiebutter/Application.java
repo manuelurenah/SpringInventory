cbbb6f39c391489686d8ec173ad95c2fd244bba8
Manuel E. Ureña <manuel.ureh@hotmail.com>
2016-10-04 00:10:40 -0400

    Added i18n support

--- src/main/java/com/cookiebutter/Application.java
+++ src/main/java/com/cookiebutter/Application.java
@@ -2,6 +2,14 @@ package com.cookiebutter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 /**
  * Created by MEUrena on 10/3/16.
@@ -9,10 +17,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
 }
--- src/main/resources/templates/login.ftl
+++ src/main/resources/templates/login.ftl
@@ -5,6 +5,12 @@
     <title>Login</title>
 </head>
 <body>
-    <h2>Hello, ${name}</h2>
+    <h2><@spring.message "hello_msg" />, ${name}</h2><br /><br />
+    <div>
+        <p><@spring.message "change_locale_msg" />
+            <a href="?lang=en"><@spring.message "default_lang_msg" /></a>|
+            <a href="?lang=es"><@spring.message "spanish_lang_msg" /></a>
+        </p>
+    </div>
 </body>
 </html>
